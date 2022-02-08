b23853bb2b690819bd76982ee0bca410791f021b
github_vusumzi-qod <github_vusumzi@qodbrains.com>
2022-02-08 13:38:52 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,93 @@
+
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+// ============================================================================================
+// card logic
+// ============================================================================================
+
+const allCards = document.querySelectorAll('.card');
+
+allCards.forEach(card => {
+    card.addEventListener('click', slideToggle)
+});
+
+function slideToggle(e){
+    e.preventDefault();
+    let slideWrapper = this.firstElementChild;
+
+    if(e.target.classList.contains('btn-right') || e.target.classList.contains('fa-long-arrow-alt-right')){
+        slideWrapper.classList.remove('slideLeft');
+        slideWrapper.classList.add('slideRight');
+    }
+
+    if(e.target.classList.contains('btn-left') || e.target.classList.contains('fa-long-arrow-alt-left')){
+        slideWrapper.classList.remove('slideRight');
+        slideWrapper.classList.add('slideLeft');
+    }
+}
+
+// =============================================================================================
+// button 
+// =============================================================================================
+
+var button = document.getElementsByClassName("first-btn")[0];
+button.addEventListener("click", function mybtn() {
+  window.location.href="/about.html";
+})
+
